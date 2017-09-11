32aa02d433b5ffac0b93a7b05b4943d4c21a5449
Dmitry Umbras <freeudv@mail.ru>
2017-09-11 11:49:49 +0300

    prettier

--- js/app.js
+++ js/app.js
@@ -5,56 +5,79 @@ const todoList = document.getElementById('todo-list');
 todoForm.addEventListener('submit', addTodoItem);
 
 function createTodoItem(title) {
-    let checkbox = createElement('input', { type: 'checkbox', className: 'checkbox', onchange: toggleTodoItem });
-    let label = createElement('label', { className: 'title' }, title);
-    let editInput = createElement('input', { type: 'text', className: 'textfield' });
-    let editButton = createElement('button', { className: 'edit', onclick: editTodoItem }, 'Изменить');
-    let deleteButton = createElement('button', { className: 'delete', onclick: deleteTodoItem }, 'Удалить');
-    let listItem = createElement('li', { className: 'todo-item' }, checkbox, label, editInput, editButton, deleteButton);
+  let checkbox = createElement('input', {
+    type: 'checkbox',
+    className: 'checkbox',
+    onchange: toggleTodoItem
+  });
+  let label = createElement('label', { className: 'title' }, title);
+  let editInput = createElement('input', {
+    type: 'text',
+    className: 'textfield'
+  });
+  let editButton = createElement(
+    'button',
+    { className: 'edit', onclick: editTodoItem },
+    'Изменить'
+  );
+  let deleteButton = createElement(
+    'button',
+    { className: 'delete', onclick: deleteTodoItem },
+    'Удалить'
+  );
+  let listItem = createElement(
+    'li',
+    { className: 'todo-item' },
+    checkbox,
+    label,
+    editInput,
+    editButton,
+    deleteButton
+  );
 
-    return listItem;
+  return listItem;
 }
 
 function addTodoItem(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let title = addInput.value;
+  let title = addInput.value;
 
-    if (title === '') {
-        return alert('Необходимо ввести название задачи.');
-    }
+  if (title === '') {
+    return alert('Необходимо ввести название задачи.');
+  }
 
-    let todoItem = createTodoItem(title);
+  let todoItem = createTodoItem(title);
 
-    todoList.appendChild(todoItem);
-    addInput.value = '';
+  todoList.appendChild(todoItem);
+  addInput.value = '';
 }
 
 function toggleTodoItem() {
-    let listItem = this.parentNode;
+  let listItem = this.parentNode;
 
-    listItem.classList.toggle('completed');
+  listItem.classList.toggle('completed');
 }
 
 function editTodoItem() {
-    let listItem = this.parentNode;
-    let title = listItem.querySelector('.title');
-    let editInput = listItem.querySelector('.textfield');
-    let isEditing = listItem.classList.contains('editing');
+  let listItem = this.parentNode;
+  let title = listItem.querySelector('.title');
+  let editInput = listItem.querySelector('.textfield');
+  let isEditing = listItem.classList.contains('editing');
 
-    if (isEditing) {
-        title.textContent = editInput.value;
-        this.textContent = 'Изменить';
-    } else {
-        editInput.value = title.textContent;
-        this.textContent = 'Сохранить';
-    }
+  if (isEditing) {
+    title.textContent = editInput.value;
+    this.textContent = 'Изменить';
+  } else {
+    editInput.value = title.textContent;
+    this.textContent = 'Сохранить';
+  }
 
-    listItem.classList.toggle('editing');
+  listItem.classList.toggle('editing');
 }
 
 function deleteTodoItem() {
-    let listItem = this.parentNode;
-    
-    todoList.removeChild(listItem);
+  let listItem = this.parentNode;
+
+  todoList.removeChild(listItem);
 }
--- js/dom.js
+++ js/dom.js
@@ -1,15 +1,15 @@
 function createElement(tag, props, ...children) {
-    let element = document.createElement(tag);
+  let element = document.createElement(tag);
 
-    Object.keys(props).forEach(key => element[key] = props[key]);
+  Object.keys(props).forEach(key => (element[key] = props[key]));
 
-    children.forEach(child => {
-        if (typeof child === 'string') {
-            child = document.createTextNode(child);
-        }
+  children.forEach(child => {
+    if (typeof child === 'string') {
+      child = document.createTextNode(child);
+    }
 
-        element.appendChild(child);
-    });
+    element.appendChild(child);
+  });
 
-    return element;
+  return element;
 }
