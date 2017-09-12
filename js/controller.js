e6ae8ba53057b572b13252906b0ecda181b698d8
Dmitry Umbras <freeudv@mail.ru>
2017-09-12 16:25:39 +0300

    'Done'

--- js/controller.js
+++ js/controller.js
@@ -3,4 +3,32 @@ class Controller {
     this.model = model;
     this.view = view;
   }
+
+  addTodo(title) {
+    const todo = this.model.add({
+      id: Date.now(),
+      title,
+      isCompleted: false
+    });
+
+    this.view.addTodo(todo);
+  }
+
+  toggleTodo(id, isCompleted) {
+    const todo = this.model.update(id, { isCompleted });
+
+    this.view.toggleTodo(todo);
+  }
+
+  editTodo(id, title) {
+    const todo = this.model.update(id, { title });
+
+    this.view.editTodo(todo);
+  }
+
+  deleteTodo(id) {
+    const todo = this.model.remove(id);
+
+    this.view.deleteTodo(todo);
+  }
 }
--- js/model.js
+++ js/model.js
@@ -1,11 +1,29 @@
 class Model {
-  constructor() {}
+  constructor(todos = [{ id: 1, title: 'Test', isCompleted: true }]) {
+    this.todos = todos;
+  }
 
-  add() {}
+  get(id) {
+    return this.todos.find(todo => todo.id == id);
+  }
 
-  get() {}
+  add(todo) {
+    this.todos.push(todo);
 
-  update() {}
+    return todo;
+  }
 
-  remove() {}
+  update(id, data) {
+    const todo = this.get(id);
+    Object.keys(data).forEach(prop => (todo[prop] = data[prop]));
+
+    return todo;
+  }
+
+  remove(id) {
+    const todo = this.get(id);
+    this.todos = this.todos.filter(todo => todo.id !== id);
+
+    return todo;
+  }
 }
